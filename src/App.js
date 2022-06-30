7b5f2082db5decc52feeac86a94ee39f27ea9083
sujon-chambugong <sujonchambugong@gmail.com>
2022-06-30 14:50:27 +0600

    added navbar in header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1 className='text-3xl'>Hello</h1>
     </div>
   );
