6ec79f9d63b95ac0f7e2cf4ad7e0ff8abb13c204
sujon-chambugong <sujonchambugong@gmail.com>
2022-07-02 05:27:18 +0600

    server link check and done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,12 @@ import { FaDumbbell, FaFacebookSquare, FaTwitter, FaYoutube } from "react-icons/
 import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
-        <footer class="footer items-center p-4 bg-neutral text-neutral-content">
-            <div class="items-center grid-flow-col">
+        <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+            <div className="items-center grid-flow-col">
                 <FaDumbbell size={50} /> 
                 <p>Copyright © 2022 - All right reserved <br /> by Sujon Chambugong</p>
             </div> 
-            <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
                 <Link to='/'><FaFacebookSquare size={40} /></Link>
                 <Link to='/'><FaYoutube size={40} /></Link>
                 <Link to='/'><FaTwitter size={40} /></Link>
--- src/components/Pages/CompletedTask.js
+++ src/components/Pages/CompletedTask.js
@@ -1,7 +1,7 @@
 import { useQuery } from 'react-query';
 
 const CompletedTask = () => {
-    const {data:tasks, isLoading} = useQuery('completed', () => fetch('https://conservative-donair-22687.herokuapp.com/completed').then(res => res.json()));
+    const {data:tasks, isLoading} = useQuery('completed', () => fetch('https://conservative-donair-22687.herokuapp.com/task/completed').then(res => res.json()));
 
     if(isLoading){
         return <p>Looding......</p>;
@@ -9,9 +9,9 @@ const CompletedTask = () => {
 
     return (
         <div className='mt-10 min-h-screen'>
-            <h1 className='text-3xl font-bold text-center mb-14'>Completed Task - {tasks.length}</h1>
+            <h1 className='text-3xl font-bold text-center mb-14'>Completed Task - {tasks?.length}</h1>
             <div className="flex justify-center">
-                <div className="grid grid-cols-1 lg:grid-cols-1">
+                <div className="grid grid-cols-1 lg:grid-cols-1 mb-5">
                     {
                         tasks?.map((task, index) => <div index={index} className="flex justify-start items-center p-2" key={task._id}>
                         <span>({index + 1})</span>. {task.title}
